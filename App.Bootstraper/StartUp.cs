946e0c73d163d7f72e5687d90f002135d3fd6af5
poriash <poriash1995@gmail.com>
2018-11-05 10:51:06 +0330

    Mapping Of RoleStore

--- App.Bootstraper/StartUp.cs
+++ App.Bootstraper/StartUp.cs
@@ -1,9 +1,14 @@
 ﻿using App.Bootstraper.Mapping;
 using App.Data.Sql.Context;
+using App.DomainModels.Entities.Identity;
 using App.DomainModels.ViewModels.Settings;
+using App.DomainServices.Identity;
+using App.DomainServices.Identity.Contracts;
 using App.DomainServices.Repositories;
 using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -23,6 +28,30 @@ namespace App.Bootstraper
             services.AddScoped<IUnitOfWork, AppDbContext>();
             services.AddTransient<IMessagesService, MessagesService>();
             services.AddTransient<PersonRepository>();
+
+            #region Identity
+            //services.AddScoped<IUserClaimsPrincipalFactory<User>, ApplicationClaimsPrincipalFactory>();
+            //services.AddScoped<UserClaimsPrincipalFactory<User, Role>, ApplicationClaimsPrincipalFactory>();
+
+            //services.AddScoped<IdentityErrorDescriber, CustomIdentityErrorDescriber>();
+
+            //services.AddScoped<IApplicationUserStore, ApplicationUserStore>();
+            //services.AddScoped<UserStore<User, Role, ApplicationDbContext, int, UserClaim, UserRole, UserLogin, UserToken, RoleClaim>, ApplicationUserStore>();
+
+            //services.AddScoped<IApplicationUserManager, ApplicationUserManager>();
+            //services.AddScoped<UserManager<User>, ApplicationUserManager>();
+
+            //services.AddScoped<IApplicationRoleManager, ApplicationRoleManager>();
+            //services.AddScoped<RoleManager<Role>, ApplicationRoleManager>();
+
+            //services.AddScoped<IApplicationSignInManager, ApplicationSignInManager>();
+            //services.AddScoped<SignInManager<User>, ApplicationSignInManager>();
+
+            services.AddScoped<IApplicationRoleStore, ApplicationRoleStore>();
+            services.AddScoped<RoleStore<Role, AppDbContext, int, UserRole, RoleClaim>, ApplicationRoleStore>();
+
+            #endregion
+
 
             //تزریق تمام repository ها با استفاده از reflection که از فضای نام آن ها استفاده میکند
             // Domain Services
