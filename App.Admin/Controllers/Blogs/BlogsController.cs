79174e2e9d847fc996700af2e53bde587a721b48
Pouriya <Pouriya@DESKTOP-80FGNLP>
2019-01-12 22:59:47 +0330

    Add Controller And Repository

---
+++ App.Admin/Controllers/Blogs/BlogsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace App.Admin.Controllers
+{
+    [Authorize]
+    public class BlogsController : Controller
+    {
+        public BlogsController
+           ()
+
+        {
+
+        }
+
+    }
+}
---
+++ App.DomainServices/Repositories/Product/ProductGalleriesRepository.cs
@@ -0,0 +1,12 @@
+﻿using App.DomainModels.ViewModels.Settings;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.DomainServices.Repositories
+{
+    public class ProductGalleriesRepository
+    {
+    }
+}
