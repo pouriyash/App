3dc0268b3c9b2479115dd6cbbde7be3cb12e4cbc
poriash <poriash1995@gmail.com>
2019-01-22 20:39:25 +0330

    dto

---
+++ App.DomainModels/Dto/BaseDto.cs
@@ -0,0 +1,37 @@
+﻿using App.DomainModels.Entities;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace App.DomainModels.Dto
+{
+    public abstract class BaseDto<TDTO,TEntity,TKey>
+        where TDTO: class, new()
+        where TEntity : BaseEntity<TKey>, new()
+    {
+        [Display(Name = "ردیف")]
+        public TKey Id { get; set; }
+
+        public TEntity ToEntity()
+        {
+            return Mapper.Map<TEntity>(CastToDerivedClass(this));
+        }
+
+        public TEntity ToEntity(TEntity entity)
+        {
+            return Mapper.Map(CastToDerivedClass(this), entity);
+        }
+
+        public static TDTO FromEntity(TEntity model)
+        {
+            return Mapper.Map<TDTO>(model);
+        }
+
+        protected TDTO CastToDerivedClass(BaseDto<TDTO, TEntity, TKey> baseInstance)
+        {
+            return Mapper.Map<TDTO>(baseInstance);
+        }
+    }
+}
---
+++ App.DomainModels/Entities/BaseEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App.DomainModels.Entities
+{
+    public abstract class BaseEntity<TKey>
+    {
+        public TKey Id { get; set; }
+    } 
+}
